7f759f1ba1344f8a52f2cd2c59c759bfb6954c97
Alicja Chruszczyk <alicja.chruszczyk@gmail.com>
2018-06-12 20:24:09 +0200

    corrections

--- modules/time.js
+++ modules/time.js
@@ -4,7 +4,7 @@ function getTime() {
 	let uptime = Math.floor(os.uptime());
 	let hours = Math.floor(uptime / 3600);
 	let minutes = Math.floor((uptime - (hours * 3600)) / 60);
-	uptime = uptime % 60;
+	let seconds = uptime % 60;
 	return hours + ' hours ' + minutes + ' minutes ' + uptime + ' seconds'
 }
 
