854566ff4535b2cf4e8d84f166f69668a84e2e11
Alicja Chruszczyk <alicja.chruszczyk@gmail.com>
2018-06-14 21:37:40 +0200

    fix time.js

--- modules/time.js
+++ modules/time.js
@@ -4,8 +4,12 @@ function getTime() {
 	let uptime = Math.floor(os.uptime());
 	let hours = Math.floor(uptime / 3600);
 	let minutes = Math.floor((uptime - (hours * 3600)) / 60);
-	uptime = uptime % 60;
-	return hours + ' hours ' + minutes + ' minutes ' + uptime + ' seconds'
+	let seconds = uptime % 60;
+	if (hours == 0) {
+		return minutes + ' minutes ' + seconds + ' seconds'
+	} else {
+		return hours + ' hours ' + minutes + ' minutes ' + seconds + ' seconds'
+	}
 }
 
 exports.time = getTime;
